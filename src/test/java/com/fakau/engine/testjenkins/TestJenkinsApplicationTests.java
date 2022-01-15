6a4e24ab3255f4b386251ab9b0656498bfe17182
Laurent Kafka M <laurentkafka123@gmail.com>
2022-01-15 18:28:35 -0500

    add test for test :D

--- src/test/java/com/fakau/engine/testjenkins/TestJenkinsApplicationTests.java
+++ src/test/java/com/fakau/engine/testjenkins/TestJenkinsApplicationTests.java
@@ -3,11 +3,35 @@ package com.fakau.engine.testjenkins;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class TestJenkinsApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+	}
+
+	@Test
+	public void testYeahGreaterThan_1999(){
+		Integer year = 2002;
+		assertThat(year).isNotNull();
+		assertThat(year).isGreaterThan(1999);
+	}
+
+	@Test
+	public void testYeahIsOdd(){
+		Integer year = 1997;
+		assertThat(year).isNotNull();
+		assertThat(year).isOdd();
+	}
+
+	@Test
+	public void testYeahIsEven(){
+		Integer year = 1998;
+		assertThat(year).isNotNull();
+		assertThat(year).isEven();
 	}
 
 }
