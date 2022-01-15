1a536f1eb675558e51861f6d3a35082ecea6b1cc
Laurent Kafka M <laurentkafka123@gmail.com>
2022-01-15 16:58:08 -0500

    init

---
+++ src/test/java/com/fakau/engine/testjenkins/TestJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakau.engine.testjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
